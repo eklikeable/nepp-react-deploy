99f321ca1a19c268e718b215e5782fe6b1158739
ekkim <eklikeable@kakao.com>
2023-01-15 15:50:51 +0900

    배포연습용

---
+++ README.md
@@ -0,0 +1,6 @@
+2023.01.15  
+nepp plus react
+
+```
+npm i -D gh-pages
+```
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>배포테스트</p>
+    </div>
+  );
+}
+
+export default App;
